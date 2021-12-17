96bfafab6043339db905aaf0417056c14f6d1b41
rubenshu <ruben.sikkens@student.hu.nl>
2021-12-17 14:40:46 +0100

    Get edit ID and edit functions

---
+++ DecorusRazorWeb/Pages/Categories/Edit.cshtml.cs
@@ -0,0 +1,39 @@
+using DecorusRazorWeb.Model;
+using DecorusWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DecorusRazorWeb.Pages.Categories
+{
+    [BindProperties]
+    public class EditModel : PageModel
+    {
+        public readonly ApplicationDbContext _db;
+
+        public Category Category { get; set; }
+
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet(int id)
+        {
+            Category = _db.Categories.Find(id);
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if(Category.Name == Category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Category.Name", "Name and Display Order can not be the same");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(Category);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("index");
+            }
+            return Page();
+        }
+    }
+}
